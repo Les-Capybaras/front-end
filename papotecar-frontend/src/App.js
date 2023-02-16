58cccf8da5e0de889fbe76cf0e405844d5655a59
Benbdlt <bordelet.benjamin@free.fr>
2023-02-16 14:01:30 +0100

    init commit with first layout

---
+++ papotecar-frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import  './assets/style/main.scss';
+import Layout from './layout/layout';
+
+function App() {
+  return (
+    <div>
+        <Layout/>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ papotecar-frontend/src/layout/layout.js
@@ -0,0 +1,13 @@
+import Header from "../components/layout/header"
+import Sidebar from "../components/layout/sidebar"
+
+export default function Layout() {
+    return (
+      <div className="">
+        <Header/>
+        <Sidebar/>
+
+        layout
+      </div>
+    )
+}
